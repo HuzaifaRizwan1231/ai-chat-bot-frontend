cb9e6af82918e5a17aed2dc21511a747e5a5cbd9
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2025-01-09 11:52:13 +0500

    refactor MessageItem and MessageList components; enhance rendering logic, add MessageItemSkeleton for loading state, and improve animation transitions

--- src/features/chat/components/MessageItem.jsx
+++ src/features/chat/components/MessageItem.jsx
@@ -7,29 +7,36 @@ const MessageItem = ({ message }) => {
   const isUser = message.sender === "user";
 
   return (
-    <motion.div
-      className={`flex ${isUser ? "justify-end" : "justify-start"} my-2`}
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.3 }}
-    >
+    <div className={`flex ${isUser ? "justify-end" : "justify-start"}`}>
       {isUser ? (
         <>
           <div className="max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl rounded-3xl px-4 py-2 bg-secondaryColorLight dark:bg-secondaryColorDark text-black dark:text-white">
-            <MarkdownRenderer message={message} classStyles={"text-base leading-loose"} />
+            <MarkdownRenderer
+              message={message}
+              classStyles={"text-base leading-loose"}
+            />
           </div>
         </>
       ) : (
         <>
-          <div className="w-full flex gap-4 rounded-3xl px-4 py-2 text-gray-800 dark:text-white">
-            <div className="w-[24px] h-[24px]">
+          <div className="w-full flex items-start gap-4 rounded-3xl px-4 py-2 text-gray-800 dark:text-white">
+            <div className="w-[24px] h-[24px] pt-1">
               <ChatBotIcon model={message.sender} />
             </div>
-            <MarkdownRenderer message={message} classStyles={"text-base leading-loose w-full overflow-x-auto"} />
+            <motion.div
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: 100 }}
+              transition={{ duration: 3 }}
+            >
+              <MarkdownRenderer
+                message={message}
+                classStyles={"text-base leading-loose w-full overflow-x-auto"}
+              />
+            </motion.div>
           </div>
         </>
       )}
-    </motion.div>
+    </div>
   );
 };
 
--- src/features/chat/components/MessageList.jsx
+++ src/features/chat/components/MessageList.jsx
@@ -2,6 +2,7 @@ import React, { forwardRef } from "react";
 import { motion } from "framer-motion";
 import MessageItem from "./MessageItem";
 import ChatBotIcon from "./icons/ChatBotIcon";
+import MessageItemSkeleton from "./skeletons/MessageItemSkeleton";
 
 const MessageList = forwardRef(({ loading, messages, selectedModel }, ref) => {
   return (
@@ -15,34 +16,25 @@ const MessageList = forwardRef(({ loading, messages, selectedModel }, ref) => {
       {messages.length === 0 ? (
         <div className="flex items-center justify-center h-full">
           <div className="text-center text-gray-500 dark:text-gray-400">
-          <h2 className="text-2xl font-bold mb-2">Welcome to AI Chatbot!</h2>
-          <p className="text-lg">
-            Start a conversation by typing a message below.
-          </p>
+            <h2 className="text-2xl font-bold mb-2">Welcome to AI Chatbot!</h2>
+            <p className="text-lg">
+              Start a conversation by typing a message below.
+            </p>
           </div>
         </div>
       ) : (
         messages.map((message) => (
-          <MessageItem selectedModel={selectedModel} key={message.id} message={message} />
+          <MessageItem
+            selectedModel={selectedModel}
+            key={message.id}
+            message={message}
+          />
         ))
       )}
 
       {/* Skeleton loader indicating response generation */}
       {loading && (
-        <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.3 }}
-        className="ps-4 text-black dark:text-white flex gap-4">
-          <div className="w-[24px]">
-          <ChatBotIcon model={selectedModel}/>
-          </div>
-          <div className="animate-pulse flex flex-col gap-2 w-full">
-            <div className="h-6 bg-secondaryColorLight dark:bg-secondaryColorDark rounded w-3/4"></div>
-            <div className="h-6 bg-secondaryColorLight dark:bg-secondaryColorDark rounded w-5/6"></div>
-            <div className="h-6 bg-secondaryColorLight dark:bg-secondaryColorDark rounded w-2/3"></div>
-          </div>
-        </motion.div>
+          <MessageItemSkeleton  selectedModel={selectedModel}/>
       )}
     </motion.div>
   );
