2f0397639fad0a087b179492b30a99847d32098c
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2025-01-14 16:17:05 +0500

    feat: implement encryption and decryption for chat API; add crypto-js dependency and refactor API calls to use encrypted data

--- src/features/chat/api/chat.api.js
+++ src/features/chat/api/chat.api.js
@@ -1,21 +1,27 @@
-import axios from "../../../config/axios.config"
+import axios from "../../../config/axios.config";
+import { decryptData, encryptData } from "../../../utils/cypto.utils";
 
-const baseURL = '/api/chat'
+const baseURL = "/api/chat";
 
-export const getResponseFromChatApiCall = async (body)=>{
-    try {
-        const response = await axios.post(`${baseURL}/completion`, body)
-        return response.data
-    } catch (e) {
-        return e
-    }
-}
+export const getResponseFromChatApiCall = async (body) => {
+  try {
+    const response = await axios.post(`${baseURL}/completion`, {
+      model: body.model,
+      text: encryptData(body.text),
+    });
+    response.data.data = decryptData(response.data.data);
+    return response.data;
+  } catch (e) {
+    return e;
+  }
+};
 
 export const trancribeAudioApiCall = async (formData) => {
-    try {
-        const response = await axios.post(`${baseURL}/transcribe`, formData);
-        return response.data;
-    } catch (e) {
-        return e;
-    }
+  try {
+    const response = await axios.post(`${baseURL}/transcribe`, formData);
+    response.data.data = decryptData(response.data.data);
+    return response.data;
+  } catch (e) {
+    return e;
+  }
 };
